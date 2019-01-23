b0e4a9dcc70f6cc42c792c7af8d375e54cd02c7a
Maciej Winnicki <mthenw@users.noreply.github.com>
2019-01-23 23:00:49 +0100

    Update base image

--- rsync/entrypoint.sh
+++ rsync/entrypoint.sh
@@ -5,11 +5,13 @@ set -e
 SSH_PATH="$HOME/.ssh"
 
 mkdir "$SSH_PATH"
+touch "$SSH_PATH/known_hosts"
 
 echo "$PRIVATE_KEY" > "$SSH_PATH/deploy_key"
 echo "$PUBLIC_KEY" > "$SSH_PATH/deploy_key.pub"
 
 chmod 700 "$SSH_PATH"
+chmod 600 "$SSH_PATH/known_hosts"
 chmod 600 "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key.pub"
 
