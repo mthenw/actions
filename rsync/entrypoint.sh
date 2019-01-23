235444aadf0d491c1e01fe4241436d1011f11111
Maciej Winnicki <mthenw@users.noreply.github.com>
2019-01-23 22:28:58 +0100

    Add rsync action

---
+++ rsync/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+SSH_PATH="$HOME/.ssh"
+
+mkdir "$SSH_PATH"
+
+echo "$PRIVATE_KEY" > "$SSH_PATH/deploy_key"
+echo "$PUBLIC_KEY" > "$SSH_PATH/deploy_key.pub"
+
+chmod 700 "$SSH_PATH"
+chmod 600 "$SSH_PATH/deploy_key"
+chmod 600 "$SSH_PATH/deploy_key.pub"
+
+eval $(ssh-agent)
+ssh-add "$SSH_PATH/deploy_key"
+
+rsync "$*"
